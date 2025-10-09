fa050220147106567b959ae4da619d97f2a195b1
miltonpaiva <miltonpaiva268@gmail.com>
2025-10-08 23:25:42 -0300

    refactor: atualização das configurações

--- config/filesystems.php
+++ config/filesystems.php
@@ -31,35 +31,32 @@ return [
     'disks' => [
 
         'local' => [
-            'driver' => 'local',
-            'root' => storage_path('app/private'),
-            'serve' => true,
-            'throw' => false,
-            'report' => false,
+            'driver'      => 'local',
+            'root'        => storage_path('app/private'),
+            'serve'       => true,
+            'throw'       => true,
+            'report'      => true,
+            'visibility'  => 'public',
+            'permissions' => [
+                'file' => [
+                    'public'  => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public'  => 0755,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'driver'     => 'local',
+            'root'       => storage_path('app/public'),
+            'url'        => env('APP_URL').'/storage',
             'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
+            'throw'      => true,
+            'report'     => true,
         ],
-
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-            'report' => false,
-        ],
-
     ],
 
     /*
