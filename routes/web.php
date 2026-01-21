44ba272b624a9398d4ebe0275d60b469584d3111
miltonpaiva <miltonpaiva@opovodigital.com>
2026-01-21 12:23:37 -0300

    fix: correção de rota do painel

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,15 @@ Route::get('/', [App\Http\Controllers\AccountController::class, 'index'])->name(
 
 Route::get('/admin', [App\Http\Controllers\AccountController::class, 'indexAdmin'])->name('index.admin');
 
-Route::get('/painel-gerencial', [App\Http\Controllers\AccountController::class, 'painelGerencial'])->name('painel.gerencial');
-Route::post('/painel-gerencial/update', action: [App\Http\Controllers\AccountController::class, 'update'])->name('painel-gerencial.update');
+Route::get('/painel-gerencial',         [App\Http\Controllers\AccountController::class, 'painelGerencial'])->name('painel.gerencial');
+Route::post('/painel-gerencial/update', [App\Http\Controllers\AccountController::class, 'update'])->name('painel-gerencial.update');
 
 Route::prefix('admin')->group(function() {
     // Produtos
     Route::get('/products/{id}', [App\Http\Controllers\ProductController::class, 'show']);
     Route::put('/products/{id}', [App\Http\Controllers\ProductController::class, 'update']);
     Route::post('/products', [App\Http\Controllers\ProductController::class, 'store'])->name('products.create');
-    
+
     // Categorias
     Route::get('/categories', [App\Http\Controllers\ProductController::class, 'getCategories'])->name('categories.list');
     Route::post('/categories', [App\Http\Controllers\ProductController::class, 'createCategory'])->name('categories.create');
