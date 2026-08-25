e85297a823445a1448b233770cf5463eae7b05b3
miltonpaiva <miltonpaiva@opovodigital.com>
2026-08-25 12:25:09 -0300

    refactor: atualização do popup inicial

--- resources/views/popups/first_access.blade.php
+++ resources/views/popups/first_access.blade.php
@@ -5,47 +5,12 @@
             BOTA O ICONE AQUI
         </div>
         <h3 class="custom-popup__title" id="promptTitle">Olá, bem vindo a MNW BLACK BEACH</h3>
-        <p class="custom-popup__message" id="promptMessage">Por favor, insira abaixo seu nome e uma senha para que só você realize seus pedidos:</p>
-        <select id="table_number" class="custom-popup__input table_number">
-            <option value="">numero da sua mesa</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
-            <option value="23">23</option>
-            <option value="24">24</option>
-            <option value="26">26</option>
-            <option value="27">27</option>
-            <option value="28">28</option>
-            <option value="29">29</option>
-            <option value="30">30</option>
-        </select>
+        <p class="custom-popup__message" id="promptMessage">Por favor, insira abaixo seu nome para poder usar nossa plataforma:</p>
+        <input type="hidden" id="table_number" value=99>
         <input type="text"   class="custom-popup__input" id="name" placeholder="Seu nome">
-        <input type="number" class="custom-popup__input" id="code" placeholder="Sua senha ex: 1234" min="0001" max="9999">
         <div class="custom-popup__actions">
-            <button class="custom-popup__btn custom-popup__btn--secondary close_popup" id="first_access_btn_cancel">
-                Cancelar
-            </button>
             <button class="custom-popup__btn custom-popup__btn--primary" id="first_access_btn_ok">
-                Fazer meu pedido
+                Confirmar
             </button>
         </div>
     </div>
