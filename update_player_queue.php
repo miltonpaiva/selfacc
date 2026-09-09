8aa8ddaa3f94435d8ab7399445801d5bbe846863
miltonpaiva <miltonpaiva@opovodigital.com>
2026-09-09 12:36:52 -0300

    feat: arquivo de execução da cron

---
+++ update_player_queue.php
@@ -0,0 +1,6 @@
+<?php
+
+// $result = file_get_contents('http://selfacc.local.com.br/api/music-queue-update');
+$result = file_get_contents('https://mnwblackbeach.com.br/api/music-queue-update');
+
+print_r($result);
